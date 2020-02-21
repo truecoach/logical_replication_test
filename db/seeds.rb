a886398bf57c0ec0e1541c962b118bf53dbfa887
Adam Steel <adamgsteel@gmail.com>
2020-02-21 11:01:12 -0700

    add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+attribute_sets = [
+  {
+    name: 'adam',
+    email: 'adam@truecoach.co'
+  }
+]
+
+attribute_sets.each do |attributes|
+  next if User.find_by(email: attributes[:email])
+
+  puts "seeding #{attributes}"
+  User.create!(attributes)
+end

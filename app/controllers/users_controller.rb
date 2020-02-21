a18ca8f5b6cec4a281b1f097aa74b02edf0c8351
Adam Steel <adamgsteel@gmail.com>
2020-02-21 16:05:47 -0700

    show slot changes in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+
+    @changes = begin
+      res = ActiveRecord::Base.connection.execute(
+        <<~SQL
+          SELECT * FROM pg_logical_slot_get_changes('slot1', NULL, NULL);
+        SQL
+      )
+      JSON.parse(res.to_json)
+    rescue StandardError => e
+      "Unable to show slot changes: #{e.message.chomp}"
+    end
   end
 
   # GET /users/1
